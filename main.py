33ef9d23f0cda60cd08f78aefb7d04e2770319ed
WildChilly <burdakoffmusic@gmail.com>
2024-11-17 23:02:38 +0300

    17/11/2025 practice 2

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ print(9 ** 0.5 * 5)
 # 2nd program
 print(99.9 > 99.8 and 1000 != 1000.1)
 
-#3rd prorgam
+#3rd program
 print(2 * 2 + 2)
 print(2 * (2 + 2))
 print(int(2 * 2 + 2) == int(2 * (2 + 2)))
@@ -34,3 +34,8 @@ print(float(h) * 10)
 
 # version 2
 print(float('123.456') * 10)
+
+#test commit lessons Git hub
+
+# 1st program
+print(9 ** 0.5 * 5)
