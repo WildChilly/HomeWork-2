7dcd34b646a3c514e255a5cc2f492c523ff2a9cf
WildChilly <burdakoffmusic@gmail.com>
2024-11-17 22:51:07 +0300

    17/11/2025 practice 2

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(99.9 > 99.8 and 1000 != 1000.1)
+
+#3rd prorgam
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(int(2 * 2 + 2) == int(2 * (2 + 2)))
+
+#4th program
+h = '123.456'
+
+print(float(h) * 10)
+
+
+# version 2
+print(float('123.456') * 10)
