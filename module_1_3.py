c5dbf82d4a6aae6df7f8987fbe6f1be288c8ed87
WildChilly <burdakoffmusic@gmail.com>
2024-11-21 14:56:38 +0300

    17/11/2025 practice 3 Rename main.py for mainHomeWork.py add homework in file - module_1_3

---
+++ module_1_3.py
@@ -0,0 +1,21 @@
+
+
+name = 'Alexey'
+
+print('Имя: ' + name)
+
+age = 28
+
+print('Возраст: ' + str(age))
+
+new_age = age + 1
+# пробовал просто с заменой переменной, но так показалось интереснее тк функция становится
+# более вариативной.
+
+
+print('Новый возраст: ' + str(new_age))
+
+is_student = True
+
+print('Является студентом Urban University:', is_student)
+
