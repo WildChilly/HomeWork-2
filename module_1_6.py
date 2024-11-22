1e4e937c55ed2458ff6996770ad814544c5a5c5a
WildChilly <burdakoffmusic@gmail.com>
2024-11-22 13:53:52 +0300

    17/11/2025 practice 3 Rename main.py for mainHomeWork.py add homework in file - module_1_3
    add homework in file module_1_6.py

---
+++ module_1_6.py
@@ -0,0 +1,8 @@
+name_app = "Pithon"
+home_work_done_count = hom = 12
+hours_spent = hs = 1.5
+mean_spent_time = float(hs / hom)
+mst = mean_spent_time
+
+
+print("Курс:", name_app + ',', 'всего задач:', str(hom) + ',', 'затрачено часов:', str(hs) + ',', 'среднее время выполнения:', str(mst) + '.')
