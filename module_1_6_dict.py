f238d6d0294457f28d971a476f7bfda33b0e3b7c
WildChilly <burdakoffmusic@gmail.com>
2024-12-01 12:51:50 +0300

    17/11/2025 practice 3 Rename main.py for mainHomeWork.py add homework in file - module_1_3
    add homework in file module_1_6.py
    add homework in file module_1-4.py
    
    add homework in file module_1_9.py
    add homework in file module_1_5.py
    add homework in file module_1_6_dict.py

---
+++ module_1_6_dict.py
@@ -0,0 +1,26 @@
+my_first_dict = mfd = {'Alexey': 'Bass guitar', 'Oleg': 'Sax', 'Anton': 'Trombone', 'Zheka': 'Drums', 'Kostya': 'Guitar', 'Jonathan':'Vocal', 'Sanya':'Sound Mixer'}
+print(mfd)
+
+print(mfd['Oleg'])
+print(mfd.get('Karpenko'))
+mfd.update({'Karpenko':'Ex Trombone', 'Artur':'Driver'})
+print(mfd)
+print(mfd.keys())
+print(mfd.values())
+
+uvolen_is_grupp = mfd.pop('Karpenko')
+
+print(uvolen_is_grupp)
+print(mfd)
+
+
+
+#Множества
+my_first_set = mfs = {1, 2, 2, 2, 'Ex Trombone', 'Ex Trombone', True, False, True}
+print(mfs)
+mfs.update({3,4})
+print(mfs)
+mfs.remove(2)
+mfs.discard(True)
+mfs.remove('Ex Trombone')
+print(mfs)
