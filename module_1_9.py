69a5a6afdc2150db05acdfaef6b50f97e2fbf8b9
WildChilly <burdakoffmusic@gmail.com>
2024-12-07 15:03:23 +0300

    17/11/2025 practice 3 Rename main.py for mainHomeWork.py add homework in file - module_1_3
    add homework in file module_1_6.py
    add homework in file module_1-4.py
    
    add homework in file module_1_9.py
    add homework in file module_1_5.py
    add homework in file module_1_6_dict.py
    
    add homework in file module_1_7_AddTask.py
    add homework in file
    module_2_2.py
    Доделал программу сравнения УРАААА

---
+++ module_1_9.py
@@ -0,0 +1,7 @@
+example = 'Перераспределение'
+
+print(example[0])
+print(example[-1])
+print(example[7:])
+print(example[::-1])
+print(example[1::2])
---
+++ module_2_2.py
@@ -0,0 +1,28 @@
+name = input('Как к вам обращаться? '
+             '(введите Ваше имя/никнейм): ')
+
+print('Привет,', name, ', я программа сравнения чисел, давай-ка всё сравним!')
+while True:
+    print('Введи свои числа ниже в появляющиеся строки, после ввода каждого числа жми "Enter" чтобы пойти дальше')
+    first = input('Введи первое число: ')
+    second = input('Введи второе число: ')
+    third = input('Введи третье число: ')
+    #while True:
+    #if len(first) == 1 and first.isalpha() or len(second) == 1 and second.isalpha() or third == 1 and third.isalpha():
+    #print('Числа это циферки :))) буквы еще не могу')
+    #break
+
+    try:
+        if first.isalpha() or second.isalpha() or third.isalpha():
+            print('Числа это циферки :))) буквы еще не могу')
+        elif first == second and first == third and second == third:
+            print('Все числа похожи, ура :) Индекс: ', 3)
+
+        elif first == second or first == third or second == third:
+            print('Есть два похожих числа, неплохо :/ Индекс', 2)
+
+        elif first != second and first != third or second != third:
+            print('Похожих совсем нет:( Индекс: ', 0)
+    except:
+
+        print('Пробуем еще?')
