9965ff34281e86dcb54d607394a7e0800dbb8a45
WildChilly <burdakoffmusic@gmail.com>
2025-01-15 21:12:53 +0300

    17/11/2025 practice 3 Rename main.py for mainHomeWork.py add homework in file - module_1_3
    add homework in file module_1_6.py
    add homework in file module_1-4.py
    
    add homework in file module_1_9.py
    add homework in file module_1_5.py
    add homework in file module_1_6_dict.py
    
    add homework in file module_1_7_AddTask.py
    add homework in file
    module_2_2.py
    Доделал программу сравнения УРАААА
    
    Добавил урок по циклу while
    
    Добавил урок по циклу for
    
    Добавил домашнее по теме а зачем нам эти матрицы? Вы сами знаете?
    
    Написал мини игру текстовую для доп задания модуль 2 "Слишком сложный шифр"
    
    Допилил чутка Шифр
    
    module 3__1.py
    Решил задание согласно условиям
    
    Module 3__2.py
    Выполнял правописание за педагогом во время лекции и выполнил задание

---
+++ Module 3__2.py
@@ -0,0 +1,58 @@
+from pkgutil import extend_path
+
+
+def parametrs(a,b,c):
+    print(a, b ,c)
+
+def function(a, b=3, c=None):
+    if c is None:
+        c = []
+        c.append(a + b)
+    print(c)
+   # elif c > 0:
+
+
+      #  print(c)
+
+
+
+
+function(4)
+function(5,b=6)
+
+function(6,4,44)
+function(9,34,33)
+
+
+
+
+def mail_sander(sender = 'university.help@gmail.com', message = 'Вы все хорошие', *, recipient):
+    #message = 'если раскомментировать, то получится принимающий аргумент в функции и все message по умолчанию будут
+    # этим текстом
+    if sender == recipient:
+        print(f'Невозможно отправить самому себе')
+        return
+
+
+    if '@' not in sender or not (sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net')):
+        print(f'Невозможно отправить письмо с адреса1 {sender} на адрес {recipient}. Сообщение: {message}')
+        print('ОШИБКА! У отправителя пропущены символы "@", ".ru", ".net", ".com"')
+    elif '@' not in recipient or not (recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net')):
+        print(f'Невозможно отправить письмо с адреса2 {sender} на адрес {recipient}. Сообщение: {message}')
+        print('ОШИБКА! У получателя пропущены символы "@", ".ru", ".net", ".com"')
+    else:
+        if sender == 'university.help@gmail.com':
+            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}. Сообщение: {message}')
+        else:
+            print(f'ВНИМАНИЕ: НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо успешно отправлено с адреса {sender} на адрес {recipient}.'
+                  f' Сообщение: {message}')
+
+mail_sander(message='Urban uchit coditb, как же horosho', recipient='killdozerbur@yandex.com')
+mail_sander(recipient='chillyband@gavmail.net')
+mail_sander(sender='university.help@gmail.cococo', recipient='killdozerbur@yande.ru')
+mail_sander(recipient='trevorgmail.ru')
+mail_sander(sender='chillybandonfire@gavmail.ru', message='Письмо от кавер группы Чилли', recipient='merries@mail.net')
+mail_sander(recipient='university.help@gmail.com')
+
+#Крч надеюсь все правильно, было много затыков со знаками препинания и НИ в ОДНОМ УРОКЕ НЕТ ОБЬЯСНЕНИЯ о способе вывода
+# принт через print(f''). в модуле номер два преподователь пользуется этим способом и очень вскользь упоминает
