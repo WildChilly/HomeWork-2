e39d687a58210a2644a9de74a8041d4745bc54fe
WildChilly <burdakoffmusic@gmail.com>
2025-01-16 19:25:05 +0300

    17/11/2025 practice 3 Rename main.py for mainHomeWork.py add homework in file - module_1_3
    add homework in file module_1_6.py
    add homework in file module_1-4.py
    
    add homework in file module_1_9.py
    add homework in file module_1_5.py
    add homework in file module_1_6_dict.py
    
    add homework in file module_1_7_AddTask.py
    add homework in file
    module_2_2.py
    Доделал программу сравнения УРАААА
    
    Добавил урок по циклу while
    
    Добавил урок по циклу for
    
    Добавил домашнее по теме а зачем нам эти матрицы? Вы сами знаете?
    
    Написал мини игру текстовую для доп задания модуль 2 "Слишком сложный шифр"
    
    Допилил чутка Шифр
    
    module 3__1.py
    Решил задание согласно условиям
    
    Module 3__2.py
    Выполнял правописание за педагогом во время лекции и выполнил задание
    Добавил возможность ввода эмейлов
    
    module 3__3.py сделал домашнее задание
    
    Module 3__4.py
    сделал домашнее задание по уроку произвольное число параметров

---
+++ Module 3__5_Recurcion.py
@@ -0,0 +1,54 @@
+def summa(n):
+    if n == 0:
+        return 0
+    else:
+        return n + summa(n - 1)
+
+
+
+print(summa(5))
+
+# Стэк вызовов, хранение в памяти информации о вызовах программы
+stack = []
+stack.append(1) # Добавляем элементы с начала двигаясь к концу
+print('Добавили элемент', stack)
+stack.append(2)
+print('Добавили элемент', stack)
+stack.append(3)
+print('Добавили элемент', stack)
+print(stack)  # Убираем элементы с конца - двигаюсь к началу
+stack.pop()
+print('Убрали элемент', stack)
+stack.pop()
+print('Убрали элемент', stack)
+stack.pop()
+print('Убрали элемент', stack)
+print(stack) # тут увидим пустой список
+
+
+# ошибка рекурсии
+# def recursion():
+#     recursion()
+# recursion()
+
+# Самостоятельная работа
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        if first != 0:
+
+            return first * get_multiplied_digits(int(str_number[1:]))
+        else:
+            return get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits(40203)
+result2 = get_multiplied_digits(402033300)
+print(result)
+print(result2)
+
+# к сожалению опять ни к чему не приуроченный пример
+# не удалось добиться вывода результата 2 до 24 по примеру. в конце получается 0
