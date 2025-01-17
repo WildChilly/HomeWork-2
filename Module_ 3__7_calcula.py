8c6f1c47eff94210ee9c619756adb74c794ba664
WildChilly <burdakoffmusic@gmail.com>
2025-01-17 18:22:12 +0300

    17/11/2025 practice 3 Rename main.py for mainHomeWork.py add homework in file - module_1_3
    add homework in file module_1_6.py
    add homework in file module_1-4.py
    
    add homework in file module_1_9.py
    add homework in file module_1_5.py
    add homework in file module_1_6_dict.py
    
    add homework in file module_1_7_AddTask.py
    add homework in file
    module_2_2.py
    Доделал программу сравнения УРАААА
    
    Добавил урок по циклу while
    
    Добавил урок по циклу for
    
    Добавил домашнее по теме а зачем нам эти матрицы? Вы сами знаете?
    
    Написал мини игру текстовую для доп задания модуль 2 "Слишком сложный шифр"
    
    Допилил чутка Шифр
    
    module 3__1.py
    Решил задание согласно условиям
    
    Module 3__2.py
    Выполнял правописание за педагогом во время лекции и выполнил задание
    Добавил возможность ввода эмейлов
    
    module 3__3.py сделал домашнее задание
    
    Module 3__4.py
    сделал домашнее задание по уроку произвольное число параметров
    
    Module 3__5_Recurcsion
    решил ваш пример, получил не совсем корректный результат
    
    Module 3__6_bulitFunc.py вручную прошел все этапы практики на видео
    
    Графический интерфейс калькулятора дауна
    
    Дописали логику для калькулятора дауна

--- Module_ 3__7_calcula.py
+++ Module_ 3__7_calcula.py
@@ -1,17 +1,62 @@
 import tkinter as tk
 from idlelib.debugger_r import wrap_info
 
+def get_values(): # функция принимающая ввод с первого и второго окошка для ввода
+    """
+    принимающая функция с экранов для ввода и возвращающая значения
+    :return:
+    """
+
+
+    num1 = int(number1_entry.get())
+    num2 = int(number2_entry.get())
+    return num1, num2
+
+def insert_values(value):
+    """
+    принимаюая функция
+    :param value:
+    :return:
+    """
+    answer_entry.delete(0, 'end')
+    answer_entry.insert(0, value)
+
+def add():
+    """
+    функция сложения
+    :return:
+    """
+    num1, num2 = get_values()
+    res = num1 + num2
+    insert_values(res)
+
+def sub():
+    num1, num2 = get_values()
+    res = num1 - num2
+    insert_values(res)
+
+def mul():
+    num1, num2 = get_values()
+    res = num1 * num2
+    insert_values(res)
+
+def div():
+    num1, num2 = get_values()
+    res = num1 / num2
+    insert_values(res)
+
+
 window = tk.Tk()
 window.title('Калькулятор')
 window.geometry('450x450')
 window.resizable(False,False)
-button_add = tk.Button(window, text='+', width=4, height=2) # создали перееменную и загнали в нее визуализацию кнопки, которая привязана к нашему окну
+button_add = tk.Button(window, text='+', width=4, height=2, command=add) # создали перееменную и загнали в нее визуализацию кнопки, которая привязана к нашему окну
 button_add.place(x=50, y=140) # отсчет начинается с левого верхнего угла x= слева на право, y = сверху вниз
-button_sub = tk.Button(window, text='-', width=4, height=2)
+button_sub = tk.Button(window, text='-', width=4, height=2, command=sub)
 button_sub.place(x=150, y=140)
-button_mul = tk.Button(window, text='*', width=4, height=2)
+button_mul = tk.Button(window, text='*', width=4, height=2, command=mul)
 button_mul.place(x=250,y=140)
-button_div = tk.Button(window, text='/', width=4, height=2)
+button_div = tk.Button(window, text='/', width=4, height=2, command=div)
 button_div.place(x=350,y=140)
 number1_entry = tk.Entry(window, width=50)
 number1_entry.place(x=50,y=45)
