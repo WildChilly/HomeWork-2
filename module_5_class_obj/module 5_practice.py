f7060a51d9db4ab149366b2b60ddbecdf9da279b
WildChilly <burdakoffmusic@gmail.com>
2025-01-24 16:00:09 +0300

    17/11/2025 practice 3 Rename main.py for mainHomeWork.py add homework in file - module_1_3
    add homework in file module_1_6.py
    add homework in file module_1-4.py
    
    add homework in file module_1_9.py
    add homework in file module_1_5.py
    add homework in file module_1_6_dict.py
    
    add homework in file module_1_7_AddTask.py
    add homework in file
    module_2_2.py
    Доделал программу сравнения УРАААА
    
    Добавил урок по циклу while
    
    Добавил урок по циклу for
    
    Добавил домашнее по теме а зачем нам эти матрицы? Вы сами знаете?
    
    Написал мини игру текстовую для доп задания модуль 2 "Слишком сложный шифр"
    
    Допилил чутка Шифр
    
    module 3__1.py
    Решил задание согласно условиям
    
    Module 3__2.py
    Выполнял правописание за педагогом во время лекции и выполнил задание
    Добавил возможность ввода эмейлов
    
    module 3__3.py сделал домашнее задание
    
    Module 3__4.py
    сделал домашнее задание по уроку произвольное число параметров
    
    Module 3__5_Recurcsion
    решил ваш пример, получил не совсем корректный результат
    
    Module 3__6_bulitFunc.py вручную прошел все этапы практики по
     видео
    
     add namespaces.py
    
    Графический интерфейс калькулятора дауна
    
    Дописали логику для калькулятора дауна
    
    Module 3__8 additional.py
    Написал дополнительное задание
    
    Module 4__1.py создали папку модуль
    создали 3 модуля
    импортировали 2 модуля фейк и тру в модуль 4
    
    add module 5_1.py создал папку для 5 модуля, создал файл с домашним заданием
    
    21/01/2025 добавил вторую часть задания в модуль 5_1
    и поигрался с обьектами в процессе урока в модуле 5_2
    
    добавил третью часть задания
    
    add module 5_practice.py
    Реализовал проверку кода на наличие нужных символов

---
+++ module_5_class_obj/module 5_practice.py
@@ -0,0 +1,37 @@
+class Database:
+    def __init__(self):
+        self.data = {}
+    def add_user(self, username, password):
+        self.data[username] = password
+        if len(password) < 6:
+            print('Пароль должен содержать не меньше 6 символов')
+
+        if (not any(char.isupper()) for char in password) or \
+            (not any(char.islower()) for char in password) or \
+            (not any(char.isdigit()) for char in password) or \
+            (not any(char.isalnum()) for char in password):# нашел самое оптимальное и лаконичное решение.
+                                                            # Мне лично всегда сложно начать с логики отрицания,
+                                                            # в голове всегда присутствует то что должно быть, но на
+                                                            # самом деле легче пойт от того чего быть не должно.
+                                                            # есть ли методики для развития мышления и понимания написания кода?
+                print('Пароль должен содержать хотя бы одну букву верхнего или нижнего регистра, '
+                      'цифру, уникальные символы - "!@#$%^&*{}[]/(\)[\]"')
+
+class User:
+    """
+    Класс пользователя содержащий атрибутыЖ логин, пароль
+    """
+    def __init__(self, username, password, password_confirm):
+        self.username = username
+        if password == password_confirm:
+            self.password = password
+
+if __name__ == '__main__':
+    database = Database()
+    while True:
+        choice = input('Приветсвую! Выберите действие: \n1 - Вход \n2 - Регистрация \n')
+        user =User(input('Введите логин: '), password := input('Введите пароль: '), password2 := input('Подтвердите пароль: '))
+        if password != password2:
+            exit()
+        database.add_user(user.username, user.password)
+        print(database.data)
