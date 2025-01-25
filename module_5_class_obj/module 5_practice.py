aa6d0ea1938d1f699bd6d1ec275f9b49b1815f8f
WildChilly <burdakoffmusic@gmail.com>
2025-01-25 17:52:35 +0300

    17/11/2025 practice 3 Rename main.py for mainHomeWork.py add homework in file - module_1_3
    add homework in file module_1_6.py
    add homework in file module_1-4.py
    
    add homework in file module_1_9.py
    add homework in file module_1_5.py
    add homework in file module_1_6_dict.py
    
    add homework in file module_1_7_AddTask.py
    add homework in file
    module_2_2.py
    Доделал программу сравнения УРАААА
    
    Добавил урок по циклу while
    
    Добавил урок по циклу for
    
    Добавил домашнее по теме а зачем нам эти матрицы? Вы сами знаете?
    
    Написал мини игру текстовую для доп задания модуль 2 "Слишком сложный шифр"
    
    Допилил чутка Шифр
    
    module 3__1.py
    Решил задание согласно условиям
    
    Module 3__2.py
    Выполнял правописание за педагогом во время лекции и выполнил задание
    Добавил возможность ввода эмейлов
    
    module 3__3.py сделал домашнее задание
    
    Module 3__4.py
    сделал домашнее задание по уроку произвольное число параметров
    
    Module 3__5_Recurcsion
    решил ваш пример, получил не совсем корректный результат
    
    Module 3__6_bulitFunc.py вручную прошел все этапы практики по
     видео
    
     add namespaces.py
    
    Графический интерфейс калькулятора дауна
    
    Дописали логику для калькулятора дауна
    
    Module 3__8 additional.py
    Написал дополнительное задание
    
    Module 4__1.py создали папку модуль
    создали 3 модуля
    импортировали 2 модуля фейк и тру в модуль 4
    
    add module 5_1.py создал папку для 5 модуля, создал файл с домашним заданием
    
    21/01/2025 добавил вторую часть задания в модуль 5_1
    и поигрался с обьектами в процессе урока в модуле 5_2
    
    добавил третью часть задания
    
    add module 5_practice.py
    Реализовал проверку кода на наличие нужных символов
    с помощью нейросети решил синтакситечску ошибку и добавил метод проверки пароля

--- module_5_class_obj/module 5_practice.py
+++ module_5_class_obj/module 5_practice.py
@@ -2,21 +2,35 @@ class Database:
     def __init__(self):
         self.data = {}
     def add_user(self, username, password):
-        self.data[username] = password
+        if self._is_valid_password(password):
+            self.data[username] = password
+            print(f'Пользователь успешно зарегистрирован {username}')
+        else:
+            print('Не верный ввод, повторите попытку')
+    def _is_valid_password(self, password):
         if len(password) < 6:
             print('Пароль должен содержать не меньше 6 символов')
+            return False
+        if (not any(char.isupper() for char in password) or
+                not any(char.islower() for char in password) or
+                not any(char.isdigit() for char in password) or
+                not any(char in "!@#$%^&*{}[]/()\\\"'" for char in password)):
+            print('Пароль должен содержать хотя бы одну букву верхнего или нижнего регистра, '
+                  'цифру и уникальные символы - "!@#$%^&*{}[]/()\\\"\'"')
+            return False
 
-        if (not any(char.isupper()) for char in password) or \
-            (not any(char.islower()) for char in password) or \
-            (not any(char.isdigit()) for char in password) or \
-            (not any(char.isalnum()) for char in password):# нашел самое оптимальное и лаконичное решение.
-                                                            # Мне лично всегда сложно начать с логики отрицания,
-                                                            # в голове всегда присутствует то что должно быть, но на
-                                                            # самом деле легче пойт от того чего быть не должно.
-                                                            # есть ли методики для развития мышления и понимания написания кода?
-                print('Пароль должен содержать хотя бы одну букву верхнего или нижнего регистра, '
-                      'цифру, уникальные символы - "!@#$%^&*{}[]/(\)[\]"') # как убрать синтаксическую ошибку, поможет ли метод raise и обозначение ошибки?
-
+        # if (not any(char.isupper()) for char in password) or \
+        #     (not any(char.islower()) for char in password) or \
+        #     (not any(char.isdigit()) for char in password) or \
+        #     (not any(char.isalnum()) for char in password):# нашел самое оптимальное и лаконичное решение.
+        #                                                     # Мне лично всегда сложно начать с логики отрицания,
+        #                                                     # в голове всегда присутствует то что должно быть, но на
+        #                                                     # самом деле легче пойт от того чего быть не должно.
+        #                                                     # есть ли методики для развития мышления и понимания написания кода?
+        #     print('Пароль должен содержать хотя бы одну букву верхнего или нижнего регистра, '
+        #           'цифру, уникальные символы - "!@#$%^&*{}[]/(\)[\]"') # как убрать синтаксическую ошибку, поможет ли метод raise и обозначение ошибки?
+        #     return False
+        return True
 class User:
     """
     Класс пользователя содержащий атрибутыЖ логин, пароль
@@ -30,8 +44,19 @@ if __name__ == '__main__':
     database = Database()
     while True:
         choice = input('Приветсвую! Выберите действие: \n1 - Вход \n2 - Регистрация \n')
-        user =User(input('Введите логин: '), password := input('Введите пароль: '), password2 := input('Подтвердите пароль: '))
-        if password != password2:
-            exit()
-        database.add_user(user.username, user.password)
+        if choice == '1':
+            login = input('Enter login: ')
+            password = input('Enter password: ')
+            if login in database.data:
+                if password == database.data[login]:
+                    print(f'Welcome, {login}')
+                    break
+            else:
+                print('Unknown user')
+        if choice == '2':
+            user = User(input('Введите логин: '), password := input('Введите пароль: '),
+                       password2 := input('Подтвердите пароль: '))
+            if password != password2:
+                continue
+            database.add_user(user.username, user.password)
         print(database.data)
